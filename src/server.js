c89dfec46837b39fbdb88591d16132ab9c7c9813
intakie <intakiea@gmail.com>
2025-10-15 01:24:49 +0300

    upd 1

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-// import dotenv from 'dotenv';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
-// dotenv.config();
+const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
+
+  app.use(cors());
+  app.use(express.json());
 
   app.use(
     pino({
@@ -16,22 +20,53 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(cors());
-
   app.get('/', (req, res) => {
-    res.json({ message: 'Server is working!' });
+    res.json({ message: 'Hello world!' });
   });
 
-  app.use('*', (req, res) => {
-    res.status(404).json({ message: 'Not found' });
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      data: contacts,
+    });
   });
 
-  const PORT = process.env.PORT || 8080;
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
 
   app.listen(PORT, (error) => {
     if (error) {
       throw error;
     }
-    console.log(`Server started on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   });
 };
